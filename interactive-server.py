20363a9ee8f652e76d0192c0ad15ec76aea86614
Sam Gardner <samuel.gardner@ttu.edu>
2023-12-31 13:14:17 -0600

    make geoviews axis

--- interactive-server.py
+++ interactive-server.py
@@ -2,18 +2,24 @@ from pyxlma.lmalib.io import read as lma_read
 from pyxlma.plot.xlma_plot_feature import color_by_time
 import xarray as xr
 import numpy as np
-from bokeh.plotting import figure, show
+from bokeh.plotting import figure
 from bokeh.models import Range1d
 import pandas as pd
 import hvplot.pandas
-import holoviews as hv
 import panel as pn
+
+import geoviews as gv
+from cartopy import crs as ccrs
 
 px_scale = 7
 plan_edge_length = 60
 hist_edge_length = 20
 
 alt_limit = Range1d(0, 20000, bounds=(0, 100000))
+
+ds, start_time = lma_read.dataset('LYLOUT_230615_200000_0600.dat.gz')
+lon_range = Range1d(ds.network_center_longitude.data - 5, ds.network_center_longitude.data + 1)
+lat_range = Range1d(ds.network_center_latitude.data - 1, ds.network_center_latitude.data + 1)
 
 def hook_hist_src_limiter(plot, element):
     init_xmax = plot.state.x_range.end
@@ -28,8 +34,11 @@ def hook_xalt_limiter(plot, element):
 def hook_xlabel_rotate(plot, element):
     plot.state.xaxis.major_label_orientation = -np.pi/2
 
-# ds is an xarray dataset
-ds, start_time = lma_read.dataset('LYLOUT_230615_200000_0600.dat.gz')
+def hook_lon_linker(plot, element):
+    plot.state.x_range = lon_range
+
+def hook_lat_linker(plot, element):
+    plot.state.y_range = lat_range
 
 ds = ds.assign_coords({'number_of_events' : ds.number_of_events.values})
 df = pd.DataFrame({
@@ -37,16 +46,16 @@ df = pd.DataFrame({
     'lat' : ds.event_latitude.values,
     'alt' : ds.event_altitude.values,
     'time' : ds.event_time.values,
-    'timefloats' : color_by_time(ds.event_time.values)[-1] # This gives me float values scaled properly for times
+    'timefloats' : color_by_time(ds.event_time.values)[-1]
 })
 
 xlim = (ds.network_center_longitude.data - 5, ds.network_center_longitude.data + 5)
 ylim = (ds.network_center_latitude.data - 1, ds.network_center_latitude.data + 1)
 zlim = (0, 20000)
 
-plan_ax = df.hvplot.points(x='lon', y='lat', c='timefloats', cmap='rainbow', rasterize=True, colorbar=False, xlim=xlim, ylim=ylim, width=px_scale*plan_edge_length, height=px_scale*plan_edge_length, responsive=False)
-lon_alt_ax = df.hvplot.points(x='lon', y='alt', c='timefloats', cmap='rainbow', rasterize=True, colorbar=False, xlim=xlim, ylim=zlim, width=px_scale*plan_edge_length, height=px_scale*hist_edge_length, responsive=False).opts(hooks=[hook_yalt_limiter])
-lat_alt_ax = df.hvplot.points(x='alt', y='lat', c='timefloats', cmap='rainbow', rasterize=True, colorbar=False, ylim=ylim, xlim=zlim, width=px_scale*hist_edge_length, height=px_scale*plan_edge_length, responsive=False).opts(hooks=[hook_xlabel_rotate, hook_xalt_limiter])
+plan_ax = df.hvplot.points(x='lon', y='lat', c='timefloats', cmap='rainbow', rasterize=True, colorbar=False, xlim=xlim, ylim=ylim, width=px_scale*plan_edge_length, height=px_scale*plan_edge_length, responsive=False, projection=ccrs.PlateCarree()).opts(hooks=[hook_lon_linker, hook_lat_linker])
+lon_alt_ax = df.hvplot.points(x='lon', y='alt', c='timefloats', cmap='rainbow', rasterize=True, colorbar=False, xlim=xlim, ylim=zlim, width=px_scale*plan_edge_length, height=px_scale*hist_edge_length, responsive=False).opts(hooks=[hook_yalt_limiter, hook_lon_linker])
+lat_alt_ax = df.hvplot.points(x='alt', y='lat', c='timefloats', cmap='rainbow', rasterize=True, colorbar=False, ylim=ylim, xlim=zlim, width=px_scale*hist_edge_length, height=px_scale*plan_edge_length, responsive=False).opts(hooks=[hook_xlabel_rotate, hook_xalt_limiter, hook_lat_linker])
 hist_ax = df.hvplot.hist(y='alt', bin_range=zlim, bins=20, invert=True, rasterize=True, xlabel='alt', ylabel='src', width=px_scale*hist_edge_length, height=px_scale*hist_edge_length, responsive=False).opts(hooks=[hook_xlabel_rotate, hook_hist_src_limiter, hook_yalt_limiter])
 alt_time = df.hvplot.points(x='time', y='alt', c='timefloats', cmap='rainbow', rasterize=True, colorbar=False, ylim=zlim, width=px_scale*(plan_edge_length+hist_edge_length), height=px_scale*hist_edge_length, responsive=False).opts(hooks=[hook_yalt_limiter])
 
