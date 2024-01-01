7399e33b342470abaa28159fe364ec04f427f671
Sam Gardner <samuel.gardner@ttu.edu>
2024-01-01 14:41:15 -0600

    shapely US counties

--- interactive-server.py
+++ interactive-server.py
@@ -9,7 +9,10 @@ import hvplot.pandas
 import panel as pn
 
 import geoviews as gv
+import geoviews.feature as gf
 from cartopy import crs as ccrs
+import shapefile
+from shapely.geometry import shape
 
 px_scale = 7
 plan_edge_length = 60
@@ -54,6 +57,9 @@ ylim = (ds.network_center_latitude.data - 1, ds.network_center_latitude.data + 1
 zlim = (0, 20000)
 
 plan_ax = df.hvplot.points(x='lon', y='lat', c='timefloats', cmap='rainbow', rasterize=True, colorbar=False, xlim=xlim, ylim=ylim, width=px_scale*plan_edge_length, height=px_scale*plan_edge_length, responsive=False, projection=ccrs.PlateCarree()).opts(hooks=[hook_lon_linker, hook_lat_linker])
+counties_shp = shapefile.Reader('ne_10m_admin_2_counties.shp').shapes()
+counties_shp = [shape(counties_shp[i]) for i in range(len(counties_shp))]
+plan_ax = gv.Path(counties_shp).opts(color='gray') * gf.borders().opts(color='black') * gf.states().opts(color='black', line_width=2) * plan_ax
 lon_alt_ax = df.hvplot.points(x='lon', y='alt', c='timefloats', cmap='rainbow', rasterize=True, colorbar=False, xlim=xlim, ylim=zlim, width=px_scale*plan_edge_length, height=px_scale*hist_edge_length, responsive=False).opts(hooks=[hook_yalt_limiter, hook_lon_linker])
 lat_alt_ax = df.hvplot.points(x='alt', y='lat', c='timefloats', cmap='rainbow', rasterize=True, colorbar=False, ylim=ylim, xlim=zlim, width=px_scale*hist_edge_length, height=px_scale*plan_edge_length, responsive=False).opts(hooks=[hook_xlabel_rotate, hook_xalt_limiter, hook_lat_linker])
 hist_ax = df.hvplot.hist(y='alt', bin_range=zlim, bins=20, invert=True, rasterize=True, xlabel='alt', ylabel='src', width=px_scale*hist_edge_length, height=px_scale*hist_edge_length, responsive=False).opts(hooks=[hook_xlabel_rotate, hook_hist_src_limiter, hook_yalt_limiter])
