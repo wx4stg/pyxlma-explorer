d83868c5b07a6f44571e8ac831e1e2ebf90f6fe0
Sam Gardner <samuel.gardner@ttu.edu>
2024-01-15 04:56:20 -0600

    split controls into old and new?

--- interactive-server.py
+++ interactive-server.py
@@ -8,10 +8,14 @@ lmae = LMADataExplorer(filename)
 limit_button = pn.widgets.Button(name='Limit to Selection', button_type='primary')
 pn.bind(lmae.limit_to_polygon, limit_button, watch=True)
 
+
+# TODO: what is 'by points', 'charge density'?, 'pts above threshold'?
 color_by_selector = pn.widgets.Select(name='Color By', options=['Time', 'Charge (User Assigned)', 'Charge (chargepol)',
                                                                 'Power (dBW)', 'Event Density', 'Log Event Density', 'Altitude'], value=lmae.color_by)
 pn.bind(lmae.change_colorby, color_by_selector, watch=True)
 
-controls = pn.Column(limit_button, color_by_selector)
+left_controls = pn.Column(color_by_selector)
+right_controls = pn.Column(limit_button)
 
-pn.Row(controls, lmae.panelHandle).servable()
+
+pn.Row(left_controls, lmae.panelHandle, right_controls).servable()
